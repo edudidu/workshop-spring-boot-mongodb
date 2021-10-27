c85691997fbe13ead67e4ff0a5df6f057c41e751
eduardo <edudidu@gmail.com>
2021-10-27 19:57:33 -0300

    Obtendo post por Id

---
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.nelioalves.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired //Instancia automaticamente
+	private PostRepository repo;
+
+	public List<Post> fundAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Post post = repo.findById(id).orElse(null);
+		if (post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado!");
+		}
+		
+		return post;
+	}
+	
+}
